4c47fabbe80d5a64a3ff14219584619da66b1545
Jérôme <jerome.guyot33@gmail.com>
2025-01-25 14:04:36 +0100

    adding bordeaux on map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,4 +1,5 @@
 var addressPoints = [
   [ "Nice, France", 43.70001, 7.25 ],
-  [ "Paris, France", 48.866669, 2.33333 ]
+  [ "Paris, France", 48.866669, 2.33333 ],
+  [ "Bordeaux, France", 44.833328, -0.56667]
 ];
