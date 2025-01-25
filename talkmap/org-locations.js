304fb02991eef978d466ae976b9c6874c343a8d4
Jérôme <jerome.guyot33@gmail.com>
2025-01-25 02:15:57 +0100

    repair typo

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,11 +1,4 @@
 var addressPoints = [
-  [ "Nice, France",
-    43.70001, 
-    7.25
-  ],
-
-  [ "Paris, France",
-    48.866669,
-    2.33333
-  ]
+  [ "Nice, France", 43.70001, 7.25 ],
+  [ "Paris, France", 48.866669, 2.33333 ]
 ];
