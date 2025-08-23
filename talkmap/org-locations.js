582b2c4d36c652a5f5ce506b155ab97572372bea
Jerome <jerome.guyot33@gmail.com>
2025-08-23 03:13:59 +0200

    correct label map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,6 +1,6 @@
 var addressPoints = [
-  [ "Nice, France", 43.70001, 7.25 ],
-  [ "Paris, France", 48.866669, 2.33333 ],
-  [ "Bordeaux, France", 44.833328, -0.56667 ],
-  ["Berlin, Germany",  52.52000660, 13.40495400 ]
+  [ "19th Mons Theoretical Computer Science Days", 43.70001, 7.25 ],
+  [ "JITA 2025", 48.866669, 2.33333 ],
+  [ "Highlights 2024", 44.833328, -0.56667 ],
+  ["Seminar of Free University of Berlin QEC subgroup",  52.52000660, 13.40495400 ]
 ];
